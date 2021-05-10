c61d197593af8a80953478ef109a6f40223113d3
Levi Schwartzberg <Levischwartzberg@gmail.com>
2021-05-09 22:25:44 -0500

    eliminated a few unneeded components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Employees from "./pages/Employees";
 import About from "./pages/About";
-import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
@@ -16,7 +15,6 @@ function App() {
           <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
           <Route exact path="/employees" component={Employees} />
-          <Route exact path="/search" component={Search} />
         </Wrapper>
         <Footer />
       </div>
